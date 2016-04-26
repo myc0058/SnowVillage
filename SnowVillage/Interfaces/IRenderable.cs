f7ea561c76fe9132c34ca6cf9f37cc221d8784d0
YoungChul Mo <myc0058@gmail.com>
2016-04-26 12:04:09 +0900

    FirstCommit

---
+++ SnowVillage/Interfaces/IRenderable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnowVillage.Interfaces
+{
+    public interface IRenderable
+    {
+        void Render();
+    }
+}
---
+++ SnowVillage/MainForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SnowVillage
+{
+    public partial class MainForm : Form
+    {
+        private Timer renderTimer = null;
+        private SnowVillage snowVillage = null;
+        private Graphics g = null;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            //코드 순서가 중요하기 때문에 주의해야함
+            ClientSize = GlobalConsts.StageSize;
+
+            g = CreateGraphics();
+
+            snowVillage = new SnowVillage();
+            snowVillage.DrawCanvas = g;
+
+            renderTimer = new Timer();
+            renderTimer.Interval = GlobalConsts.RenderTimerInterval;
+            renderTimer.Tick += RenderTimer_Tick;
+            renderTimer.Start();
+        }
+
+        private void RenderTimer_Tick(object sender, EventArgs e)
+        {
+            snowVillage.Render();
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+        }
+    }
+}
